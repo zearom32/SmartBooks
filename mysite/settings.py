36561debfabaa85cd100415376876c2d35efe735
zearom32 <zearom32@gmail.com>
2015-04-18 14:32:50 +0000

    add static file

--- mysite/settings.py
+++ mysite/settings.py
@@ -8,12 +8,14 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from os import environ
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates')]
+TEMPLATE_DIRS = [os.path.join(BASE_DIR,'templates'),os.path.join(BASE_DIR,'site-packages.zip/django/contrib/admin/templates')]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
